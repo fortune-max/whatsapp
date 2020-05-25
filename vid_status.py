a9b118adfc073ee71db0a84b28145b3eb99b77d1
lordfme <fortmax120@gmail.com>
2020-05-25 20:08:07 +0100

    Updated whitelist

--- vid_status.py
+++ vid_status.py
@@ -5,7 +5,7 @@ import sqlite3, sys, os, time, re
 # set WhatsApp Database directory here, uses current directory on failure
 DB_DIR = "/data/data/com.whatsapp/databases/"
 
-whitelist = {"2349030455845", "2348159186070", "2348139319159", "2348022219174", "2348170021596", "2348109143064"} # Modify this to include numbers to never disable
+whitelist = {"2349030455845", "2348159186070", "2348139319159", "2348022219174", "2348170021596", "2348109143064", "233542912743", "2349090775802", "2347081584968", "2348105073022"} # Modify this to include numbers to never disable
 
 STATUS_PRFX = "STATUS_MSG" # Message caption to discriminate status vs regular msg
 
